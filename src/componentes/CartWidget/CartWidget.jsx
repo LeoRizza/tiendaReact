a270721e7cce26c055d8cc8581c30ffa87917729
lrizza <leonardorizza@hotmail.com>
2023-07-04 16:25:58 -0300

    segunda entrega3

--- src/componentes/CartWidget/CartWidget.jsx
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -3,7 +3,7 @@ import './CartWidget.css'
 const CartWidget = () => {
     const imgCarrito = "../img/carrito.png";
     return (
-        <div>
+        <div className='carritoDiv'>
             <img className='carrito' src={imgCarrito} alt="carrito de compras" />
             <strong> 10 </strong>
         </div>
--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,31 +1,41 @@
+import { useState } from 'react';
 import CartWidget from '../CartWidget/CartWidget';
 import { Link, NavLink } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
-    return (
+    const [menuVisible, setMenuVisible] = useState(false);
 
+    const cerrarMenu = () => {
+        setMenuVisible(false);
+    };
+
+    const abrirMenu = () => {
+        setMenuVisible(true);
+    };
+
+    return (
         <header>
             <Link className='logo' to="/">
                 <h1>SABROSON</h1>
             </Link>
 
-
-            <nav>
-                <ul>
+            <nav className={`nav ${menuVisible ? 'visible' : ''}`}>
+                <button className='cerrarMenu' onClick={cerrarMenu}><img className='icono' src="../img/close.png" alt="close" /></button>
+                <ul className='navl'>
                     <li>
-                        <NavLink className="estiloCat" to="/categoria/2"> Pizzas </NavLink>
+                        <NavLink className="estiloCat" to="/categoria/2">Pizzas</NavLink>
                     </li>
                     <li>
-                        <NavLink className="estiloCat" to="/categoria/3"> Empanadas </NavLink>
+                        <NavLink className="estiloCat" to="/categoria/3">Empanadas</NavLink>
                     </li>
                 </ul>
+                <CartWidget />
             </nav>
-            
-            <CartWidget/>
+
+            <button className='abrirMenu' onClick={abrirMenu}><img className='icono' src="../img/burger.png" alt="burger" /></button>
         </header>
+    );
+};
 
-    )
-}
-
-export default NavBar
+export default NavBar;
